8994481efb8b1873e9e1aa6b953f700598fc36e2
Alexandru Ion <alexandruion@Alexandrus-iMac.local>
2023-04-03 18:55:42 +0200

    firebase update

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyD8kQKwQbPbei3cXvKpvqTdxAMaiJHTdz0",
-  authDomain: "firechat-app-71069.firebaseapp.com",
-  projectId: "firechat-app-71069",
-  storageBucket: "firechat-app-71069.appspot.com",
-  messagingSenderId: "247340543400",
-  appId: "1:247340543400:web:7a8c79fc51171504b7c19d",
+  apiKey: "AIzaSyDtGjKiGGlCxbe6qLq2_Mi8kUrSvPTlhrw",
+  authDomain: "react-chat-application-382019.firebaseapp.com",
+  projectId: "react-chat-application-382019",
+  storageBucket: "react-chat-application-382019.appspot.com",
+  messagingSenderId: "163346322449",
+  appId: "1:163346322449:web:c19737fd8a413a7b398db0"
 });
 
 const db = firebaseApp.firestore();
